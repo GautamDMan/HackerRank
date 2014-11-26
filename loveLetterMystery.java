f19c31b69274a1c67986daac6cc00deaf11f3ec9
Gautam Divekar <gautamdivekar9@live.com>
2014-11-26 09:41:41 +0530

    Update loveLetterMystery.java

--- loveLetterMystery.java
+++ loveLetterMystery.java
@@ -2,19 +2,19 @@ Enter file contents here
 
 int getLoveLetter(String input) {
 
-int count = 0;
-int firstChar = 0;
-int lastChar = (input.length() - 1);
+	int count = 0;
+	int firstChar = 0;
+	int lastChar = (input.length() - 1);
 
-while( firstChar < = lastChar ){
+	while( firstChar < = lastChar ){
 
-	if (input.charAt(firstChar) > input.charAt(lastChar))
-		count += (int) input.charAt(firstChar) - input.charAt(lastChar);
-	else if (input.charAt(firstChar) < input.charAt(lastChar))
-		count += (int) input.charAt(lastChar) - input.charAt(firstChar);
+		if (input.charAt(firstChar) > input.charAt(lastChar))
+			count += (int) input.charAt(firstChar) - input.charAt(lastChar);
+		else if (input.charAt(firstChar) < input.charAt(lastChar))
+			count += (int) input.charAt(lastChar) - input.charAt(firstChar);
 
-	++firstChar;
-	--lastChar;
-	}
-return count;
+		++firstChar;
+		--lastChar;
+		}
+	return count;
 }
