225bb329b48e0b9f6aab53c7f7dc6444c3def556
Gautam Divekar <gautamdivekar9@live.com>
2014-11-27 15:24:07 +0530

    Create isAnaPali.java
    partially thought about it .. might be that the length is more than 1 .. then it cannot be a palindrome

---
+++ isAnaPali.java
@@ -0,0 +1,30 @@
+Enter file contents here
+
+int isAnaPali(String input) {
+
+    int lCount = 0, rCount = 0;
+    int finalLength = 0;
+    while(lCount < input.length()) {
+        rCount = lCount;
+
+        while (rCount < input.length()) {
+            if(input.charAt(rCount) == input.charAt(lCount) && lCount == rCount) {
+	            StringBuilder myName = new StringBuilder(input);
+	            myName.setCharAt(lCount, "");
+            }
+            else if(input.charAt(rCount) == input.charAt(lCount))             {
+        	    StringBuilder myName = new StringBuilder(input);
+	            myName.setCharAt(lCount, "");
+	            myName.setCharAt(rCount, "");
+            }
+            ++rCount;
+        }
+        ++lCount;
+    }
+
+    finalLength = input.length();
+    if(finalLength > 1)
+    	return NO;
+    else 
+    	return yes;
+}
