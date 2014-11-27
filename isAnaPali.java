9d2d5523c05e9d358581492ec500e1fba258c737
Gautam Divekar <gautamdivekar9@live.com>
2014-11-27 17:22:16 +0530

    Update isAnaPali.java

--- isAnaPali.java
+++ isAnaPali.java
@@ -1,30 +1,28 @@
 Enter file contents here
 
-int isAnaPali(String input) {
+int isAnaPali(String input2) {
 
-    int lCount = 0, rCount = 1;
-    int finalLength = 0;
-    while(lCount < input.length()) {
-        rCount = lCount;
-
-        while (rCount < input.length()) {
-            if(input.charAt(rCount) == input.charAt(lCount) && lCount == rCount) {
-	            StringBuilder myName = new StringBuilder(input);
-	            myName.setCharAt(lCount, "");
-            }
-            else if(input.charAt(rCount) == input.charAt(lCount))             {
-        	    StringBuilder myName = new StringBuilder(input);
-	            myName.setCharAt(lCount, "");
-	            myName.setCharAt(rCount, "");
-            }
-            ++rCount;
-        }
-        ++lCount;
-    }
-
-    finalLength = input.length();
-    if(finalLength > 1)
-    	return NO;
-    else 
-    	return yes;
+	String input = input2;
+	int lCount = 0, rCount = 1;
+	int finalLength = 0;
+	while (lCount < input.length()) {
+		rCount = input.length();
+		while (rCount > lCount) {
+			if (input.charAt(rCount) == input.charAt(lCount)) {
+				StringBuilder myName = new StringBuilder(input);
+				myName.setCharAt(lCount, "");
+				myName.setCharAt(rCount, "");
+			} else --rCount;
+		}++lCount;
+	}
+	finalLength = input.length();
+	if (finalLength > 0 && finalLength % 2 == 0) return NO;
+	else if (finalLength > 1 && finalLength % 2 == 1) return NO;
+	else return yes;
 }
+
+
+
+
+
+
