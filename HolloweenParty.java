be6e979cd2cbd42049c9b2311ca69c03192b7253
Gautam Divekar <gautamdivekar9@live.com>
2014-12-17 17:06:16 +0530

    Create HolloweenParty.java
    still don't know.. what is going on !!

---
+++ HolloweenParty.java
@@ -0,0 +1,10 @@
+//Enter file contents here
+
+int HolloweenParty(int cuts) {
+	int l = m = 0;
+	if (cuts % 2 = 1) {
+		l = n / 2 + n % 2;
+		m = n / 2;
+		return l * m;
+	} else return (cuts / 2) * (cuts / 2);
+}
