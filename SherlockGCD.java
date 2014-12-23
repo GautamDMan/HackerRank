0a8de96effe582d2e4727d003fc5cd00f2a519d2
Gautam Divekar <gautamdivekar9@live.com>
2014-12-23 18:36:24 +0530

    Create SherlockGCD.java
    nope nope nope

---
+++ SherlockGCD.java
@@ -0,0 +1,14 @@
+//Enter file contents here
+
+
+int SherlockGCD(int[] arr) {
+
+	for (int i = 0; i <= arr.length(), i++) {
+
+		for (int j = 0; j <= arr.length, j++) {
+
+			if (arr[i] % arr[j] == 0) return 1;
+		}
+	}
+	return 0;
+}
